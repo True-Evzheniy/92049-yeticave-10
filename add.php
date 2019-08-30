9086a449dcfa7ab388dc829de5b6cae42496a5af
Eugene Artamonov <eugene.artamonov@liflandgaming.com>
2019-08-30 22:56:27 +0300

    Add lot creation page

---
+++ add.php
@@ -0,0 +1,154 @@
+<?php
+require_once('./init.php');
+$layout_data += [
+    'title' => 'Добавление лота',
+];
+$errors = [];
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $lot = $_POST;
+
+    $required_fields = [
+        'name',
+        'description',
+        'start_price',
+        'expiry_date',
+        'bet_step',
+        'category',
+    ];
+
+    foreach ($required_fields as $field) {
+        if (empty($_POST[$field])) {
+            $errors[$field] = 'Поле не заполнено';
+        };
+    }
+    $rules = [
+        'name' => function ($value) {
+            return validate_correct_length($value);
+        },
+        'description' => function ($value) {
+            return validate_correct_length($value);
+        },
+        'start_price' => function ($value) {
+            return validate_positive_integer($value);
+        },
+        'expiry_date' => function ($value) {
+            return validate_date($value);
+        },
+        'bet_step' => function($value) {
+            return validate_positive_integer($value);
+        },
+    ];
+
+    foreach ($_POST as $key => $value) {
+        if (isset($rules[$key]) && !isset($errors[$key])) {
+            $rule = $rules[$key];
+            $errors[$key] = $rule($_POST[$key]);
+        }
+    }
+
+    check_user_file($errors, $lot);
+    $errors = array_filter($errors);
+    if(count($errors)) {
+        $add_page = include_template('add-lot.php', compact('navigation', 'categories', 'errors'));
+        $layout_data += [
+            'main_content' => $add_page,
+        ];
+        print(include_template('layout.php', $layout_data));
+        die();
+    }
+
+    process_user_file($lot);
+
+    $sql = 'INSERT INTO lots (creation_date, name, description, picture, start_price, expiry_date, bet_step, creator, category)
+                VALUES (NOW(), ?, ?, ?, ?, ?, ?, 1, ?)';
+
+    $stmt = db_get_prepare_stmt($link, $sql, array(
+        $lot['name'],
+        $lot['description'],
+        $lot['picture'],
+        $lot['start_price'],
+        $lot['expiry_date'],
+        $lot['bet_step'],
+        $lot['category'],
+    ));
+    $res = $stmt->execute();
+    if ($res) {
+        $id = $stmt->insert_id;
+        header("Location: lot.php?id=" . $id);
+    }
+}
+
+$add_page = include_template('add-lot.php', compact('navigation', 'categories', 'errors'));
+
+$layout_data += [
+    'main_content' => $add_page,
+];
+
+print(include_template('layout.php', $layout_data));
+
+function validate_correct_length($str, $min = 1, $max = 128)
+{
+    $len = strlen($str);
+    if ($len < $min or $len > $max) {
+        return "Значение должно быть от $min до $max символов";
+    }
+}
+
+function validate_positive_integer($number)
+{
+    if (!is_numeric($number) || (intval($number) != floatval($number)) || intval($number) <= 1) {
+        return 'Значение должно быть целым и больше нуля';
+    }
+}
+
+function validate_date($date)
+{
+    if (!is_date_valid($date)) {
+        return 'Укажите дату в формате ГГГГ-ММ-ДД';
+    }
+    $input = new DateTime($date);
+    $tomorrow = new DateTime('tomorrow');
+    if($input < $tomorrow) {
+        return 'Введите дату не позднее ' . $tomorrow->format('Y-m-d');
+    }
+}
+
+function check_user_file(&$errors, &$lot) {
+    if(isset($_FILES['userfile']) && file_exists($_FILES['userfile']['tmp_name'])){
+        $tmp_name = $_FILES['userfile']['tmp_name'];
+        $mime_type = mime_content_type($tmp_name);
+        if(!check_mime_type($mime_type)) {
+            $errors['userfile'] = 'Загрузите изображение в формате png или jpg';
+        }
+    } else {
+        $errors['userfile'] = 'Загрузите изображение лота';
+    }
+}
+
+function process_user_file(&$lot) {
+    $tmp_name = $_FILES['userfile']['tmp_name'];
+    $mime_type = mime_content_type($tmp_name);
+    $file_name = uniqid() . get_extension_by_mime($mime_type);
+    $path = 'uploads/' . $file_name;
+    $lot['picture'] = $path;
+    move_uploaded_file($tmp_name, $path);
+}
+
+function get_post_val($name)
+{
+    return $_POST[$name] ?? '';
+}
+function get_extension_by_mime($type) {
+    $map = [
+        'image/png' => '.png',
+        'image/jpeg' => '.jpg'
+    ];
+    return $map[$type];
+}
+
+function check_mime_type($mime_type, $allowed_types = ['image/png', 'image/jpeg'])
+{
+    return in_array($mime_type, $allowed_types);
+}
+
---
+++ templates/add-lot.php
@@ -0,0 +1,63 @@
+<?php
+    function invalid_class($name, $errors) {
+        return (isset($errors[$name])) ? 'form__item--invalid' : '';
+    }
+?>
+<link href="../css/flatpickr.min.css" rel="stylesheet">
+<main>
+    <?= $navigation; ?>
+    <form class="form form--add-lot container <?=count($errors) ? 'form--invalid' : '';?>" action="/add.php" method="post" enctype="multipart/form-data"> <!-- form--invalid -->
+        <h2>Добавление лота</h2>
+        <div class="form__container-two">
+            <div class="form__item <?=invalid_class('name', $errors);?>"> <!-- form__item--invalid -->
+                <label for="lot-name">Наименование <sup>*</sup></label>
+                <input id="lot-name" type="text" name="name" placeholder="Введите наименование лота" value="<?= get_post_val('name'); ?>">
+                <span class="form__error"><?= $errors['name'] ?? ''?></span>
+            </div>
+            <div class="form__item <?=invalid_class('category', $errors);?>">
+                <label for="category">Категория <sup>*</sup></label>
+                <select id="category" name="category">
+                    <option value="">Выберите категорию</option>
+                    <?php foreach ($categories as $category): ?>
+                        <option value="<?= $category['id']; ?>" <?=get_post_val('category') == $category['id'] ? 'selected' : '' ?>><?= $category['name']; ?></option>
+                    <?php endforeach; ?>
+                </select>
+                <span class="form__error"><?= $errors['category'] ?? ''?></span>
+            </div>
+        </div>
+        <div class="form__item form__item--wide <?=invalid_class('description', $errors);?>">
+            <label for="message">Описание <sup>*</sup></label>
+            <textarea id="message" name="description" placeholder="Напишите описание лота"><?= get_post_val('description'); ?></textarea>
+            <span class="form__error"><?= $errors['description'] ?? ''?></span>
+        </div>
+        <div class="form__item form__item--file <?=invalid_class('userfile', $errors);?>">
+            <label>Изображение <sup>*</sup></label>
+            <div class="form__input-file">
+                <input class="visually-hidden" type="file" id="lot-img" value="" name="userfile">
+                <label for="lot-img">
+                    Добавить
+                </label>
+            </div>
+            <span class="form__error"><?= $errors['userfile'] ?? ''?></span>
+        </div>
+        <div class="form__container-three">
+            <div class="form__item form__item--small <?=invalid_class('start_price', $errors);?>">
+                <label for="lot-rate">Начальная цена <sup>*</sup></label>
+                <input id="lot-rate" type="text" name="start_price" placeholder="0" value="<?= get_post_val('start_price'); ?>">
+                <span class="form__error"><?= $errors['start_price'] ?? ''?></span>
+            </div>
+            <div class="form__item form__item--small <?=invalid_class('bet_step', $errors);?>">
+                <label for="lot-step">Шаг ставки <sup>*</sup></label>
+                <input id="lot-step" type="text" name="bet_step" placeholder="0" value="<?= get_post_val('bet_step'); ?>">
+                <span class="form__error"><?= $errors['bet_step'] ?? ''?></span>
+            </div>
+            <div class="form__item <?=invalid_class('expiry_date', $errors);?>">
+                <label for="lot-date">Дата окончания торгов <sup>*</sup></label>
+                <input class="form__input-date" id="lot-date" type="text" name="expiry_date" placeholder="Введите дату в формате ГГГГ-ММ-ДД" value="<?= get_post_val('expiry_date'); ?>">
+                <span class="form__error"><?= $errors['expiry_date'] ?? ''?></span>
+            </div>
+        </div>
+        <span class="form__error form__error--bottom"><?=count($errors) ? 'Пожалуйста, исправьте ошибки в форме.' : '';?></span>
+        <button type="submit" class="button">Добавить лот</button>
+    </form>
+</main>
